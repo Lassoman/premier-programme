ebb55ee940426f701d0d615e945c49b12134b95a
lasso <lassoman85@gmail.com>
2021-11-18 15:18:20 +0100

    2_Conditions-If-Else

--- exemple.js
+++ exemple.js
@@ -1,19 +1,38 @@
 
 
-//  FONCTION POUR CALCULER IMC
+//Condition IF ELSE
 
-function calculerIMC(poids, taille){
+// let heure = 13;
 
-    // Calculer la taille en mètres
-    let tailleCalculee      = Math.pow((taille / 100), 2);
-    let resultat            = poids / Math.pow((taille / 100), 2);
+//heure == 11
+//heure != 11  verifie si heure est différent de 11
+//heure < 11
+//heure > 11
+//heure <= 11
+//heure >= 11
 
-    return resultat;
+
+// if(heure < 12){
+//     console.log('C\'est le matin ')
+// }
+// else if(heure <=18){
+//     console.log("c'est l'apres-midi")
+// }
+// else{
+//     console.log('C\'est la soirée')
+// }
+
+// age
+// si -18 : vous n'estes pas majeur
+// si age entre 18 et 20 : vous etes majeur en france
+// si +21: vous etes majeur partout, à vous les casinos!
+
+let age = 21
+
+if(age<18){
+    console.log("Vous êtes pas majeur")
+}else if(age>= 18 && age<=20){
+    console.log("Vous etes majeur en France")
+}else{
+    console.log("vous etes majeur partout, à vous les casinos!")
 }
-
-let poids  = prompt("Quel est votre poids ? (en kg)");
-let taille  = prompt("Quel est votre taille? (en cm");
-
-
-
-alert(calculerIMC(poids, taille));
