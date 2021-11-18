2d73e3ff8b82a310ca299c28de6e681b77ddb0ed
lasso <lassoman85@gmail.com>
2021-11-18 20:42:34 +0100

    10_Try-And-Catch

--- exemple.js
+++ exemple.js
@@ -1,21 +1,52 @@
-//LES BOUCLES DO-WHILE
+// LES EXCEPTIONS
 
 
 
-//les boucles do-while sont identiques au boucle while sauf que la condition a l'interieu s'execute au moins 1 foois
+// if(){
 
-//1ER EXEMPLE
+// }else{
 
-// do{
-//     console.log('test')
-// }while (false) 
+// }
 
-// ici meme si on a mit false la boucle s'execute une premiere fois
+// le "if et else" que l'on connaît deviens try et catch
+// try {
+//     //javascript va chercher si dans ce block il y a eu une erreur
+//     alert(hello);
+// } catch(error){
+//     //si il y a une erreur dans le block d'en haut Js va executer les instruction qui sont dans le block catch
+//     alert("la variable Hello n'existe pas");
+// }
+
+
+
 
 do {
+    try {
+    
+        var recompense = prompt("Choisissez une récompense : épée, arc, haches");
+        var degats;
+    
+        switch(recompense) {
+            case 'épée':
+                degats = 40;
+                break;
+            case 'arc':
+                degats = 30;
+                break;
+            case 'haches':
+                degats = 20;
+                break;
+            default:
+                throw new Error("Vous ne pouvez pas tricher")
+        }
+        alert("Vous avez choisi : "+ recompense+ "(" + degats + " degats ).");
+    } catch(error){
+        alert(error);
+    }
+    finally{
+        alert('fin du programme')
+    }
+} while (recompense == "" || recompense == null);
 
-    var prenom = prompt('Quel est votre prénom ?');
-
-} while(prenom == "" || prenom == null)
-
-alert('Bonjour ' + prenom);
+// finally = finction qui permet d'afficher un code meme dans une fonction apres un return
+//
