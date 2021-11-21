f6491fdbfce5ef603dff11ae8d1c89833d92d705
lasso <lassoman85@gmail.com>
2021-11-21 13:51:27 +0100

    les Tableaux

--- exemple.js
+++ exemple.js
@@ -1,14 +1,4 @@
+// LES TABLEAU
 
 
-
-
-function somme(nombre) {
-    if(nombre == 1){
-        return 1;
-    }
-
-    return nombre + somme(nombre -1);
-}
-
-console.log(somme(2));
-//ON AURAIT PU REALISER CE TIMER AVEC UNE BOUCLE FOR
+let monTableau = ['un','deux','trois'];
