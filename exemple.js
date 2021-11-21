461c22b59b7f50f79a9bc1a1634467748bce3994
lasso <lassoman85@gmail.com>
2021-11-21 13:59:04 +0100

    les Tableau a plusieurs dimensions

--- exemple.js
+++ exemple.js
@@ -1,4 +1,30 @@
-// LES TABLEAU
+// LES TABLEAU en JS
 
+
+let monTableau = new Array('un','deux','trois');
+
+let monTableau = Array('un','deux','trois');
 
 let monTableau = ['un','deux','trois'];
+
+
+
+// L'ancienne maniere de déclarer des tableau
+
+
+let monTableau2D = new Array(
+    
+Array('lasso','mohamed','vassiriki'),
+Array('fofana','sonco','bilel')
+
+);
+
+//La nouvelle maniere de déclarer des tableaux
+let monTableau2D = [
+    
+['lasso','mohamed','vassiriki'],
+['fofana','sonco','bilel']
+
+];
+
+
