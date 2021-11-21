7140135a172f1a4ffaa6b223d7474514ee330d77
lasso <lassoman85@gmail.com>
2021-11-21 14:01:48 +0100

    les Tableau associatifs

--- exemple.js
+++ exemple.js
@@ -1,30 +1,8 @@
-// LES TABLEAU en JS
+// LES TABLEAU en  AssociatifJS
+;
 
-
-let monTableau = new Array('un','deux','trois');
-
-let monTableau = Array('un','deux','trois');
-
-let monTableau = ['un','deux','trois'];
-
-
-
-// L'ancienne maniere de déclarer des tableau
-
-
-let monTableau2D = new Array(
-    
-Array('lasso','mohamed','vassiriki'),
-Array('fofana','sonco','bilel')
-
-);
-
-//La nouvelle maniere de déclarer des tableaux
-let monTableau2D = [
-    
-['lasso','mohamed','vassiriki'],
-['fofana','sonco','bilel']
-
-];
-
-
+let monTableauAssociatif = {
+    'prenom' : 'mark',
+    'nom' : 'zuckenberg',
+    'poste' : 'PDG de Facebook',
+}
