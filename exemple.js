49f06be81f73b3d6aa37f2d099ddf0221b7003aa
lasso <lassoman85@gmail.com>
2021-11-22 14:31:11 +0100

    tableaux associatif

--- exemple.js
+++ exemple.js
@@ -1,8 +1,26 @@
-// LES TABLEAU en  AssociatifJS
-;
+// let monTableau = new Array('un', 'deux', 'trois');
+// let monTableau = Array('un', 'deux', 'trois');
+let monTableau = ['un', 'deux', 'trois', 'quatre'];
+
+// console.log(monTableau[0])
+console.log(monTableau[monTableau.length - 1]);
+
+// let monTableau2D = new Array(
+//     Array('Mark', 'Jeff', 'Bill'),
+//     Array('Zuckerberg', 'Bezos', 'Gates')  
+// );
+
+let monTableau2D = [
+    ['Mark', 'Jeff', 'Bill'],
+    ['Zuckerberg', 'Bezos', 'Gates']
+];
+
+console.log(monTableau2D[1][2]);
 
 let monTableauAssociatif = {
-    'prenom' : 'mark',
-    'nom' : 'zuckenberg',
-    'poste' : 'PDG de Facebook',
+    'prenom' : 'Mark',
+    'nom'    : 'Zuckerberg',
+    'poste'  : 'PDG de Facebook'
 };
+
+console.log(monTableauAssociatif['poste']);
